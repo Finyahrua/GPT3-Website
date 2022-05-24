b49db188d2ff3dd77a770f9954b0616ac956ab44
finyahrua <finaddy@gmail.com>
2022-05-24 16:00:47 +0300

    navbar complete

--- src/App.js
+++ src/App.js
@@ -9,8 +9,23 @@ import {
   WhatGPT3,
 } from "./containers";
 import { Cta, Brand, Navbar } from "./components";
+import "./App.css"
 function App() {
-  return <div>Omilife</div>;
+  return (
+    <div className="App">
+      <div className="gradient_bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
